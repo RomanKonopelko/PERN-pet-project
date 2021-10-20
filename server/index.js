6045a5d4bdc195c926074217591fa02994067339
RomanKonopelko <romank761@gmail.com>
2021-10-20 13:23:38 +0300

    init/feat: PERN basis, add models connect postgreSQL db

---
+++ server/index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+require("dotenv").config();
+const sequelize = require("./db");
+const PORT = process.env.PORT || 5000;
+const models = require("./models/models");
+const app = express();
+
+const start = async () => {
+  try {
+    await sequelize.authenticate();
+    await sequelize.sync();
+    app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
