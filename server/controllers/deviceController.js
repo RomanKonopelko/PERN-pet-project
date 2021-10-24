431fab763b8d0ba8f7406fdd6baf34a83caaef1d
RomanKonopelko <romank761@gmail.com>
2021-10-24 19:15:37 +0300

    feat: add device controller and pagination

--- server/controllers/deviceController.js
+++ server/controllers/deviceController.js
@@ -1,6 +1,41 @@
+const { Device } = require("../models/models");
+const ApiError = require("../error/ApiError");
+const uuid = require("uuid");
+const path = require("path");
+
 class DeviceController {
-  async create(req, res) {}
-  async getAll(req, res) {}
+  async create(req, res, next) {
+    try {
+      const { name, price, brandId, typeId, info } = req.body;
+      const { img } = req.files;
+      let filename = uuid.v4() + ".jpg";
+      img.mv(path.resolve(__dirname, "..", "static", filename));
+      const device = await Device.create({ name, price, brandId, typeId, filename });
+      return res.json(device);
+    } catch (error) {
+      next(ApiError.badRequest(e.message));
+    }
+  }
+  async getAll(req, res) {
+    const { brandId, typeId, limit, page } = req.query;
+    page = page || 1;
+    limit = limit || 9;
+    let offset = page * limit - limit;
+    let devices;
+    if (!brandId && !typeId) {
+      devices = await Device.findAndCountAll();
+    }
+    if (brandId && !typeId) {
+      devices = await Device.findAndCountAll({ where: { brandId }, limit, offset });
+    }
+    if (!brandId && typeId) {
+      devices = await Device.findAndCountAll({ where: { typeId }, limit, offset });
+    }
+    if (brandId && typeId) {
+      devices = await Device.findAndCountAll({ where: { brandId, typeId }, limit, offset });
+    }
+    return res.json(devices);
+  }
   async getOne(req, res) {}
 }
 
--- server/routes/typeRouter.js
+++ server/routes/typeRouter.js
@@ -2,7 +2,7 @@ const Router = require("express");
 const typeController = require("../controllers/typeController");
 const router = new Router();
 
-router.post("/");
-router.get("/");
+router.post("/", typeController.create);
+router.get("/", typeController.getAll);
 
 module.exports = router;
